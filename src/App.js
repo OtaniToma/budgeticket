b849e8f0636764884398b327560e9c49646ad93e
Toma Otani <otani.toma@gmail.com>
2020-07-11 16:16:27 -0700

    set up redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { signInAction } from './reducks/users/actions';
 
 function App() {
+  const dispatch = useDispatch();
+  const selector = useSelector((state) => state);
+
+  console.log(selector.users)
+
   return (
     <>
-      BudgeTicket
+      BudgeTicket<br />
+      <button onClick={() => dispatch(signInAction({ uid: '0001', username: 'toma'}))}>Sign In</button>
     </>
   );
 }
