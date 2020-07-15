c14af85e64d82be5715f4e67932956cd69bf6278
Toma Otani <otani.toma@gmail.com>
2020-07-15 12:56:34 -0700

    add redux devtools

--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ import { ConnectedRouter } from "connected-react-router";
 import * as History from "history";
 import { MuiThemeProvider } from "@material-ui/core";
 import { theme } from "./assets/theme";
+import { devToolsEnhancer } from 'redux-devtools-extension'
 
 const history = History.createBrowserHistory();
-export const store = createStore(history);
+export const store = createStore(history, devToolsEnhancer());
 
 ReactDOM.render(
   <Provider store={store}>
