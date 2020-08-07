67bda961aa1de40fdbc22bea24c6e49a1d61fe06
Toma Otani <otani.toma@gmail.com>
2020-08-07 12:41:23 -0700

    add env file

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_DEV_API_KEY,
+  authDomain: "budgeticket.firebaseapp.com",
+  databaseURL: "https://budgeticket.firebaseio.com",
+  projectId: "budgeticket",
+  storageBucket: "budgeticket.appspot.com",
+  messagingSenderId: process.env.REACT_APP_DEV_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_DEV_APP_ID,
+};
