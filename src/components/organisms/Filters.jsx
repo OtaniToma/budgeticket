2036a231705b342da30708b69b0f66d4ce6c98e0
Toma Otani <otani.toma@gmail.com>
2020-08-07 10:37:07 -0700

    some fix

--- src/components/organisms/Filters.jsx
+++ src/components/organisms/Filters.jsx
@@ -1,22 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import StopList from "../molecules/StopList";
 import Sort from "../molecules/Sort";
 import AirlineList from "../molecules/AirlineList";
 import Divider from "@material-ui/core/Divider";
-import {
-  getCarriers,
-  getCurrencies,
-  getPlaces,
-  getQuotes
-} from "../../reducks/flights/selectors";
+import { getCarriers, getQuotes } from "../../reducks/flights/selectors";
 import { useSelector } from "react-redux";
 
 const Filters = ({ setSortType, filterStops, filterAirlines }) => {
   const selector = useSelector((state) => state);
 
   const carriers = getCarriers(selector);
-  const currencies = getCurrencies(selector);
-  const places = getPlaces(selector);
   const quotes = getQuotes(selector);
 
   const _setSortType = (props) => {
