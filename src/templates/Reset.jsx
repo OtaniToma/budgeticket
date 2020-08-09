92fdfc107ed384ba2a042068d62f4eecbd5e2d80
Toma Otani <otani.toma@gmail.com>
2020-08-08 23:54:08 -0700

    add formik to signup, add paper and box

--- src/templates/Reset.jsx
+++ src/templates/Reset.jsx
@@ -1,41 +1,71 @@
-import React, { useCallback, useState } from "react";
-import { TextInput, Button } from "../components/atoms";
+import React from "react";
 import { resetPassword } from "../reducks/users/operations";
 import { useDispatch } from "react-redux";
 import { push } from "connected-react-router";
 import Box from '@material-ui/core/Box';
+import { Formik, Form, Field } from 'formik';
+import { TextField } from 'formik-material-ui';
+import { Button, LinearProgress } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
 
 const Reset = () => {
   const dispatch = useDispatch();
 
-  const [email, setEmail] = useState("");
-
-  const inputEmail = useCallback(
-    (e) => {
-      setEmail(e.target.value);
-    },
-    [setEmail]
-  );
-
   return (
     <>
-      <h2>Reset Password</h2>
-      <TextInput
-        fullWidth={true}
-        label={"Email"}
-        multiline={false}
-        required={true}
-        rows={1}
-        value={email}
-        type={"email"}
-        onChange={inputEmail}
-      />
-      <Box p={1} bgcolor="background.paper" />
-      <Button
-        label={"Reset"}
-        color="primary"
-        onClick={() => dispatch(resetPassword(email))}
-      />
+      <Paper elevation={3}>
+        <Box p={2} bgcolor="background.paper">
+          <h2>Reset Password</h2>
+          <Formik
+            initialValues={{
+              email: ''
+            }}
+            validate={values => {
+              const errors = {};
+              if (!values.email) {
+                errors.email = 'Required';
+              } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+              ) {
+                errors.email = 'Invalid email address';
+              }
+              return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                setSubmitting(false);
+                dispatch(resetPassword(values.email))
+              }, 500);
+            }}
+          >
+            {({ submitForm, isSubmitting }) => (
+              <Form>
+                <Field
+                  component={TextField}
+                  name="email"
+                  type="email"
+                  label="Email"
+                  fullWidth={true}
+                  multiline={false}
+                  required={true}
+                  rows={1}
+                />
+                {isSubmitting && <LinearProgress />}
+                <Box p={1} bgcolor="background.paper" />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  disabled={isSubmitting}
+                  onClick={submitForm}
+                >
+                  Submit
+          </Button>
+              </Form>
+            )}
+          </Formik>
+        </Box>
+      </Paper>
+      &nbsp;
       <p onClick={() => dispatch(push("/signin"))}>Back to Sign In</p>
     </>
   );
