494a705a5e927bff6459490c942c51bca2cbf3e8
Toma Otani <otani.toma@gmail.com>
2020-08-08 23:21:39 -0700

    add formik-material-ul

--- src/templates/SignIn.jsx
+++ src/templates/SignIn.jsx
@@ -2,96 +2,80 @@ import React, { useCallback, useState } from "react";
 import { useDispatch } from "react-redux";
 import { push } from "connected-react-router";
 import { signIn } from "../reducks/users/operations";
-import { TextInput, Button } from "../components/atoms";
 import Box from '@material-ui/core/Box';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field } from 'formik';
+import { TextField } from 'formik-material-ui';
+import { Button, LinearProgress } from '@material-ui/core';
+
 
 const SignIn = () => {
   const dispatch = useDispatch();
 
-  const [email, setEmail] = useState(""),
-    [password, setPassword] = useState("");
-
-  const inputEmail = useCallback(
-    (e) => {
-      setEmail(e.target.value);
-    },
-    [setEmail]
-  );
-
-  const inputPassword = useCallback(
-    (e) => {
-      setPassword(e.target.value);
-    },
-    [setPassword]
-  );
-
   return (
     <div>
       <h2>Sign In</h2>
-
       <Formik
-       initialValues={{ email: '', password: '' }}
-       validate={values => {
-         const errors = {};
-         if (!values.email) {
-           errors.email = 'Required';
-         } else if (
-           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-         ) {
-           errors.email = 'Invalid email address';
-         }
-         return errors;
-       }}
-       onSubmit={(values, { setSubmitting }) => {
-         setTimeout(() => {
-           const data = JSON.stringify(values, null, 2);
-           setEmail(data.email)
-           setPassword(data.password)
-           setSubmitting(false);
-           dispatch(signIn(email, password))
-         }, 400);
-       }}
-     >
-       {({ isSubmitting }) => (
-         <Form>
-           <Field type="email" name="email" />
-           <ErrorMessage name="email" component="div" />
-           <Field type="password" name="password" />
-           <ErrorMessage name="password" component="div" />
-           <button type="submit" disabled={isSubmitting}>
-             Submit
-           </button>
-         </Form>
-       )}
-     </Formik>
-
-      <TextInput
-        fullWidth={true}
-        label={"Email"}
-        multiline={false}
-        required={true}
-        rows={1}
-        value={email}
-        type={"email"}
-        onChange={inputEmail}
-      />
-      <TextInput
-        fullWidth={true}
-        label={"Password"}
-        multiline={false}
-        required={true}
-        rows={1}
-        value={password}
-        type={"password"}
-        onChange={inputPassword}
-      />
-      <Box p={1} bgcolor="background.paper" />
-      <Button
-        label={"Sign In"}
-        color="primary"
-        onClick={() => dispatch(signIn(email, password))}
-      />
+      initialValues={{
+        email: '',
+        password: '',
+      }}
+      validate={values => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        }
+        if (!values.password) {
+          errors.password = 'Required';
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          setSubmitting(false);
+          dispatch(signIn(values.email, values.password))
+        }, 500);
+      }}
+    >
+      {({ submitForm, isSubmitting }) => (
+        <Form>
+          <Field
+            component={TextField}
+            name="email"
+            type="email"
+            label="Email"
+            fullWidth={true}
+            multiline={false}
+            required={true}
+            rows={1}
+          />
+          <Box p={1} bgcolor="background.paper" />
+          <Field
+            component={TextField}
+            type="password"
+            label="Password"
+            name="password"
+            fullWidth={true}
+            multiline={false}
+            required={true}
+            rows={1}
+          />
+          {isSubmitting && <LinearProgress />}
+          <Box p={1} bgcolor="background.paper" />
+          <Button
+            variant="contained"
+            color="primary"
+            disabled={isSubmitting}
+            onClick={submitForm}
+          >
+            Submit
+          </Button>
+        </Form>
+      )}
+    </Formik>
       <p onClick={() => dispatch(push("/signup"))}>Create new account</p>
       <p onClick={() => dispatch(push("/signin/reset"))}>Reset password</p>
     </div>
