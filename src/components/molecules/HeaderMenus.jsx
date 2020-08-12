d72ca3d45d42ad8795536cc24b4a39510a07bec2
Toma Otani <otani.toma@gmail.com>
2020-08-12 12:39:32 -0700

    *move getTicketsInLiked to parent component

--- src/components/molecules/HeaderMenus.jsx
+++ src/components/molecules/HeaderMenus.jsx
@@ -4,63 +4,20 @@ import IconButton from "@material-ui/core/IconButton";
 import Badge from "@material-ui/core/Badge";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import MenuIcon from "@material-ui/icons/Menu";
-import { getIsSignedIn, getUserId, getTicketsInLiked } from "../../reducks/users/selectors";
+import { getIsSignedIn, getUserId } from "../../reducks/users/selectors";
 import { push } from "connected-react-router";
-import { fetchTicketsInLiked } from '../../reducks/users/operations';
-import { db } from '../../firebase/index';
 
 const HeaderMenus = (props) => {
   const dispatch = useDispatch();
   const selector = useSelector((state) => state);
-  const uid = getUserId(selector);
   const isSignedIn = getIsSignedIn(selector);
-  let ticketsInLiked = getTicketsInLiked(selector);
-
-  console.log(ticketsInLiked)
-
-  useEffect(() => {
-    const unsubscribe = db.collection('users').doc(uid).collection('liked')
-      .onSnapshot(snapshots => {
-        snapshots.docChanges().forEach(change => {
-          const ticket = change.doc.data();
-          const changeType = change.type;
-
-          switch (changeType) {
-            case 'added':
-
-              console.log(ticketsInLiked)
-              console.log(ticket)
-              console.log([...ticketsInLiked, ticket])
-              
-              dispatch(fetchTicketsInLiked([...ticketsInLiked, ticket]));
-              break;
-            case 'modified':
-              dispatch(fetchTicketsInLiked(ticketsInLiked.map(item => {
-                if (item.likedId === change.doc.id) {
-                  return ticket
-                }
-                return item
-              })));
-              break;
-            case 'removed':
-              dispatch(fetchTicketsInLiked(ticketsInLiked.filter(item =>
-                item.likedId !== change.doc.id
-              )));
-              break;
-            default:
-              break;
-          }
-        })
-      })
-    return () => unsubscribe()
-  }, []);
 
   return (
     <>
       {isSignedIn && (
         <div>
           <IconButton onClick={() => dispatch(push("/user/liked"))}>
-            <Badge badgeContent={ticketsInLiked.length} color="secondary">
+            <Badge badgeContent={props.ticketNumbers} color="secondary">
               <FavoriteIcon />
             </Badge>
           </IconButton>
--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { push } from "connected-react-router";
 import { makeStyles } from "@material-ui/core/styles";
@@ -7,10 +7,11 @@ import Typography from "@material-ui/core/Typography";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import { getIsSignedIn, getUsername } from "../../reducks/users/selectors";
+import { getIsSignedIn, getUserId, getUsername, getTicketsInLiked } from "../../reducks/users/selectors";
 import HeaderMenus from "../molecules/HeaderMenus";
 import ClosableDrawer from "./ClosableDrawer";
-import { useCallback } from "react";
+import { fetchTicketsInLiked } from '../../reducks/users/operations';
+import { db } from '../../firebase/index';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -34,11 +35,54 @@ const Header = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
   const selector = useSelector((state) => state);
-  const isSignedIn = getIsSignedIn(selector);
+  const uid = getUserId(selector);
   const username = getUsername(selector);
+  const isSignedIn = getIsSignedIn(selector);
+  let ticketsInLiked = getTicketsInLiked(selector);
+
+  useEffect(() => {
+    if (isSignedIn) {
+      const unsubscribe = db.collection('users').doc(uid).collection('liked')
+        .onSnapshot(snapshots => {
+
+          console.log(snapshots)
+          
+          snapshots.docChanges().forEach(change => {
+            const ticket = change.doc.data();
+            const changeType = change.type;
+
+            switch (changeType) {
+              case 'added':
+
+                console.log(ticketsInLiked)
+                console.log(ticket)
+                console.log([...ticketsInLiked, ticket])
+
+                dispatch(fetchTicketsInLiked([...ticketsInLiked, ticket]));
+                break;
+              case 'modified':
+                dispatch(fetchTicketsInLiked(ticketsInLiked.map(item => {
+                  if (item.likedId === change.doc.id) {
+                    return ticket
+                  }
+                  return item
+                })));
+                break;
+              case 'removed':
+                dispatch(fetchTicketsInLiked(ticketsInLiked.filter(item =>
+                  item.likedId !== change.doc.id
+                )));
+                break;
+              default:
+                break;
+            }
+          })
+        })
+      return () => unsubscribe()
+    }
+  }, [ticketsInLiked])
 
   const [open, setOpen] = useState(false);
-
   const handleDrawerToggle = useCallback(
     (event) => {
       if (
@@ -74,10 +118,12 @@ const Header = () => {
                   Hi, {username}
                 </Typography>
               </div>
-              <HeaderMenus handleDrawerToggle={handleDrawerToggle} />
+              <HeaderMenus
+                handleDrawerToggle={handleDrawerToggle}
+                ticketNumbers={ticketsInLiked.length}
+              />
             </>
           )}
-
 
           {!isSignedIn &&
             <IconButton onClick={() => dispatch(push("/signin"))}>
