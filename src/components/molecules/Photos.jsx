bcabcd9c5574f1999312310a088d7014023546dc
Toma Otani <otani.toma@gmail.com>
2020-08-21 13:42:05 -0700

    show photos

--- src/components/molecules/Photos.jsx
+++ src/components/molecules/Photos.jsx
@@ -3,6 +3,9 @@ import { useSelector } from "react-redux";
 import { getPlaces } from "../../reducks/flights/selectors";
 import Unsplash, { toJson } from 'unsplash-js';
 import { db } from "../../firebase";
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
 
 const Photos = () => {
   const selector = useSelector((state) => state);
@@ -31,12 +34,33 @@ const Photos = () => {
 
   console.log(photos)
 
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+      overflow: 'hidden',
+      backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+      width: '100%',
+      height: 'auto',
+    },
+  }));
+  const classes = useStyles();
+
   return (
     <>
       {photos && (
-        photos.map(photo => {
-          return <img src={photo.urls.thumb} />
-        })
+        <div className={classes.root}>
+        <GridList cellHeight={150} className={classes.gridList} cols={2}>
+          {photos.map((photo) => (
+            <GridListTile key={photo.id} cols={1}>
+              <img src={photo.urls.thumb} alt={photo.description} />
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
       )}
     </>
   );
